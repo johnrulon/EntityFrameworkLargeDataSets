7eea6ad81c7cd35d3632be0ead5454542aa14c08
John <johnrulon@gmail.com>
2016-11-08 22:39:33 -0700

    Adding initial solution and all files - Copying from VSTS repo

---
+++ EntityFrameworkWithLargeDataSets/DataIntegrationTests/Repositories/ProductRepositoryIntegration2Tests.cs
@@ -0,0 +1,53 @@
+﻿namespace DataIntegrationTests.Repositories
+{
+	using Data.Models;
+	using Data.Repositories;
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	[TestClass]
+	public class ProductRepository2IntegrationTests
+	{
+		[TestMethod]
+		public void GetProduct()
+		{
+			// arrange
+			int productId = 316; // blade
+			var productRepo = new ProductRepository();
+
+			// act
+			Product result = productRepo.GetProduct(productId);
+
+			// assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void GetProductWithStandardInformation()
+		{
+			// arrange
+			int productId = 316; // blade
+			var productRepo = new ProductRepository2();
+
+			// act
+			Product result = productRepo.GetProductWithStandardInformation(productId);
+
+			// assert
+			Assert.IsNotNull(result);
+		}
+
+		[TestMethod]
+		public void GetProductWithDetailedInformation()
+		{
+			// arrange
+			int productId = 316; // blade
+			var productRepo = new ProductRepository2();
+
+			// act
+			Product result = productRepo.GetProductWithDetailedInformation(productId);
+
+			// assert
+	//		Assert.IsNotNull(result);
+		}
+	}
+}
